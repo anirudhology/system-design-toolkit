e40c5c6217b42449e989944a0e3189729b1bb6c3
anirudhology <anirudhology@gmail.com>
2024-08-14 06:46:14 +0530

    Update Leaky Bucket algorithm

--- src/rate_limiting/leaky_bucket.py
+++ src/rate_limiting/leaky_bucket.py
@@ -5,10 +5,10 @@ from threading import Lock
 class LeakyBucket:
     def __init__(self, capacity: int, leak_rate: float):
         """
-        Initialize the leaky bucket
+        Initialize the leaky bucket.
 
-        :param capacity: Maximum number of request that can be processed
-        :param leak_rate: Number of tokens that leak per unit of time
+        :param capacity: Maximum number of requests that can be processed.
+        :param leak_rate: Number of tokens that leak per unit of time.
         """
         if capacity <= 0 or leak_rate <= 0:
             raise ValueError("Capacity or leak rate should be positive")
@@ -24,33 +24,34 @@ class LeakyBucket:
 
     def get_available_tokens(self) -> int:
         """
-        Get current water level (number of tokens available) in the bucket
-        :return: available tokens
+        Get current water level (number of tokens available) in the bucket.
+
+        :return:Currently available tokens.
         """
         with self.lock:
-            self.leak()  # Ensure that the number of tokens is up-to-date
+            self.__leak()  # Ensure that the number of tokens is up-to-date
             return self.tokens
 
     def add_tokens(self, amount: int) -> bool:
         """
-        Add tokens to the bucket
+        Add tokens to the bucket.
 
-        :param amount: amount of tokens to be added (number of incoming requests)
-        :return: True, if request was added successfully, False otherwise
+        :param amount: amount of tokens to be added (number of incoming requests).
+        :return: True, if request was added successfully, False otherwise.
         """
         if amount < 0:
-            raise ValueError("Cannot add negative requests")
+            raise ValueError("Cannot add negative tokens")
 
         with self.lock:
-            self.leak()  # First, leak tokens based on the time passed
+            self.__leak()  # First, leak tokens based on the time passed
 
             if self.tokens + amount <= self.capacity:
                 self.tokens += amount
                 return True
 
-            return False
+            return False  # We are not allowing partial addition of tokens
 
-    def leak(self):
+    def __leak(self):
         """
         Simulate the leaking of the bucket based on the elapsed time
         This method is not thread-safe and should be called within a lock
